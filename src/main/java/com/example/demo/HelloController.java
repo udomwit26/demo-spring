c90d82f131aae6cc241d784671b89a019f2ced32
Your Name <you@example.com>
2021-02-02 22:10:36 +0700

    inint

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/home")
+	public String home() {
+		
+		return "hello tong";
+	}
+	
+	@GetMapping("users")
+	public List getUser() {
+		
+		List users = new ArrayList();
+		
+		for(int i=0;i<10;i++) {
+			User user = new User();
+			user.setName("name"+i);
+			user.setLastName("lastName"+i);
+			user.setAge(i);
+			users.add(user);
+		}
+		
+		return users;
+	}
+	
+}
